9bafc88d746da5f2c758768dd6dcd2c5e01bb2a1
itkorea-ktw <ktw1633@gmail.com>
2022-10-28 17:07:35 +0900

    10.28

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 const app = express();
 app.set('port',process.env.PORT || 3000);
 
@@ -25,16 +27,14 @@ app.use(session({
     name:'session-cookie',
 }));
 
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
 app.use((req,res,next)=>{
-  console.log('모든요청에다실행됩니다.');
-  next();
+    res.status(404).send('Not Found');
 });
-app.get('/',(req,res,next)=>{
-  console.log('get/요청에서만 실행됩니다');
-  next();
-}, (req,res) =>{
-  throw new Error('에러는 에러처리 미들웨어로 갑니다')
-});
+
+
 
 app.use((err,rwq,res,next)=>{
   console.error(err);
