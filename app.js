4da421c4526d733711aa37e866e22b3708e90920
itkorea-ktw <ktw1633@gmail.com>
2022-12-29 14:19:59 +0900

    에러처리미들웨어

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8888;
 
-app.get('/',(req,res)=> {
-  res.send('HELLO EXPRESS');
+
+
+
+/*전처리 미들웨어*/
+app.use((req,res,next)=>{
+  console.log('모든 요청에 다 실행됩니다.');
+  next();
 });
+/*라우트 미들웨어*/
+app.get('/',(req,res,next)=>{
+  console.log('GET / 요청에서만 실행됩니다');
+  next();
+},(req,res)=>{
+  throw new Error('에러는 에러 처리 미들웨어로 갑니다 ')
+})
+
+/*에러처리미들웨어*/ 
+app.use((err,req,res,next)=>{
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+
+
+
 
 
 app.listen(port,() =>{
