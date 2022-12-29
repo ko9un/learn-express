1bf5c5fe08f51d353c84b587d0f81fd65d0599f0
itkorea-ktw <ktw1633@gmail.com>
2022-12-29 14:11:03 +0900

    app.js 추가

--- app.js
+++ app.js
@@ -1,53 +1,13 @@
 const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session =require('express-session');
-const dotenv = require('dotenv');
-const path = require('path');
 
-dotenv.config();
 const app = express();
-app.set('port',process.env.PORT || 3000);
+const port = process.env.PORT || 8888;
 
-app.use(morgan('dev'));
-app.use('/',express.static(path.join(__dirname,'public')));
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie:{
-        httpOnly: true,
-        secure: false,
-    },
-    name:'session-cookie',
-}));
-
-app.use((req,res,next)=>{
-  console.log('모든요청에다실행됩니다.');
-  next();
-});
-app.get('/',(req,res,next)=>{
-  console.log('get/요청에서만 실행됩니다');
-  next();
-}, (req,res) =>{
-  throw new Error('에러는 에러처리 미들웨어로 갑니다')
+app.get('/',(req,res)=> {
+  res.send('HELLO EXPRESS');
 });
 
-app.use((err,rwq,res,next)=>{
-  console.error(err);
-  res.status(500).send(err.message);
-})
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/index.html'));
+app.listen(port,() =>{
+  console.log(`http://localhost:${port} 서버 대기중 `);
 });
-
-app.listen(app.get('port'),() =>{
-    let port = app.get('port');
-    console.log(`http://localhost:${port} 에서 대기중`);
-
-});
-
